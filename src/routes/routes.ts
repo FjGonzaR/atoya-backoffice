ddec1acbc01b58dbb2dd2651da2251bf0e431d66
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2020-04-09 14:18:27 -0500

    Added routes syntax

---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+export enum Routes {
+    FORM = '/form/:id',
+    LOGIN = '/login',
+    SIGNUP = '/signup',
+    ALL_FORMS = '/forms',
+    SEND_TO_CLIENT = '/form/:id/send',
+    DOWNLOAD = 'form/:id/download',
+    CREATE_REMINDER = '/reminder',
+    UPDATE_REMINDER = '/reminder/:id',
+    ALL_REMINDERS = '/reminders'
+}
