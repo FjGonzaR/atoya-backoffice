cfaf90ae40e1045ba4e26720c36632b448eb0ef0
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2020-04-09 09:07:06 -0500

    Initial dependencies

---
+++ atoya-front/src/components/Atoya.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Atoya extends Component {
+    render() {
+        return (
+            <div>
+                Atoya
+            </div>
+        )
+    }
+}
---
+++ atoya-front/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Atoya from './components/Atoya';
+ReactDOM.render(<Atoya/>,document.getElementById('root'));
