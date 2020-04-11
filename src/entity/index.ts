0a5b44d50bd51dc8181267a5158cb38f73c9b69e
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2020-04-11 02:26:50 -0500

    No mas

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,15 +1,8 @@
-import {User} from './User';
-import {Client} from './Client';
-import {Form} from './Form';
-import {Material} from './Material';
-import {PlanningOrder} from './PlanningOrder';
-import {ServiceControl} from './ServiceControl';
+import { User } from "./User";
+import { Client } from "./Client";
+import { Form } from "./Form";
+import { Material } from "./Material";
+import { PlanningOrder } from "./PlanningOrder";
+import { ServiceControl } from "./ServiceControl";
 
-export {
-    User,
-    Client,
-    Form,
-    Material,
-    PlanningOrder,
-    ServiceControl
-}
+export { User, Client, Form, Material, PlanningOrder, ServiceControl };
