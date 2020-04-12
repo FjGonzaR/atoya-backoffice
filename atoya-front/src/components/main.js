56757edce3784f58b16f6402441926936724db15
cristian <cc.pena@uniandes.edu.co>
2020-04-11 21:09:53 -0500

    package.json fixed and main finished

--- atoya-front/src/components/main.js
+++ atoya-front/src/components/main.js
@@ -13,15 +13,57 @@ import "./main.css";
 
 
 class Main extends Component {
-  state = {
-    fecha: new Date(),
-  };
+  constructor() {
+    super();
+    this.state = {
+      fecha: new Date(),
+      forms: [],
+      formsJSX: undefined
+    };
+    this.renderForms = this.renderForms.bind(this);
+  }
 
-  downloadPDF = () => {
+  componentDidMount() {
+    axios.get("https://atoya-app.herokuapp.com/forms", { headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem("token") } })
+      .then(res => {
+        var forms = [];
+        res.data.map((e) => {
+          forms.push({ id: e.id, client: e.client, created_at: new Date(e.created_at) });
+        });
+        this.setState({ forms });
+        this.setState({ formsJSX:this.renderForms()});
+      })
+  }
+
+  renderForms = () => {
+    var ret = [];
+    for (let index = 0; index < this.state.forms.length; index++) {
+      const element = this.state.forms[index];
+      ret.push(
+        <ListGroup.Item key={element.id}>
+          <Row className="justify-content-center text-center">
+            <Col xs={8}>
+              <h1>{element.client.enterprise + " - " + element.client.name + " - " + element.created_at.getDate()+"/"+(element.created_at.getMonth()+1)+"/"+element.created_at.getFullYear()}</h1>
+            </Col>
+            <Col xs={2}>
+              <Button onClick={(e) => this.downloadPDF(e)} id={`a${element.id}`} className="btn atoyaButton">Descargar PDF</Button>
+            </Col>
+            <Col xs={2}>
+              <Button onClick={(e) => this.sendEmail(e)} id={`b${element.id}`} className="btn atoyaButton">Enviar correo</Button>
+            </Col>
+          </Row>
+        </ListGroup.Item>
+      );
+    }
+    return ret;
+  }
+
+  downloadPDF = (e) => {
+    e.preventDefault();
     if (localStorage.getItem("token") != null) {
-      axios.get("http://localhost:5000/form/2bc623e3-0e6f-459f-9814-a087ce1cc961/download", { headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem("token") }, responseType: 'blob' })
+      axios.get(`https://atoya-app.herokuapp.com/form/${e.target.id.substring(1,e.target.id.length)}/download`, { headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem("token") }, responseType: 'blob' })
         .then(res => {
-          const pdfBlob = new Blob([res.data], {type:'application/pdf'});
+          const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
           saveAs(pdfBlob, "newPdf.pdf");
         })
         .catch(err => {
@@ -30,6 +72,24 @@ class Main extends Component {
     }
     else {
       console.log("NO HAY TOKEN VALIDO");
+      this.props.history.push("/");
+    }
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault();
+    if (localStorage.getItem("token") != null) {
+      axios.get(`https://atoya-app.herokuapp.com/form/${e.target.id.substring(1,e.target.id.length)}/send`, { headers: { 'Content-Type': 'application/json', 'Authorization': localStorage.getItem("token") }})
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+    else {
+      console.log("NO HAY TOKEN VALIDO");
+      this.props.history.push("/");
     }
   }
 
@@ -39,30 +99,18 @@ class Main extends Component {
         <Container>
           <br />
           <br />
-          <ListGroup className="listaEducacion">
-            <ListGroup.Item action>
-              <Row className="justify-content-center text-center">
-                <Col xs={8}>
-                  <h1>04/02/2020 - Empresa1</h1>
-                </Col>
-                <Col xs={2}>
-                  <Button variant="primary" onClick={this.downloadPDF}>Descargar PDF</Button>
-                </Col>
-                <Col xs={2}>
-                  <Button variant="secondary">Enviar correo</Button>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-          </ListGroup>
-          <br />
-          <br />
           <Row className="justify-content-center text-center">
             <Col>
               <Link to="/form">
-                <Button variant="primary">Crear Formulario</Button>
+                <Button className="btn atoyaButton">Crear Formulario</Button>
               </Link>
             </Col>
           </Row>
+          <br />
+          <br />
+          <ListGroup className="listaEducacion">
+            {this.state.formsJSX}
+          </ListGroup>
         </Container>
       </>
     );
