b00fe71f0c77f44df86206e613b0ee6306037879
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2020-05-13 15:08:36 -0500

    Solved email stuff

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,16 +1,31 @@
-import nodemailer from 'nodemailer';
-import { EmailContent } from '../types/form';
-import '../../config';
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+import { EmailContent } from "../types/form";
+import "../../config";
+const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
+export const sendEmail = async (payload: EmailContent) => {
+  const OAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    OAUTH_PLAYGROUND
+  );
+  OAuth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+  const accessToken = await OAuth2Client.getAccessToken();
 
-
-export const sendEmail = async(payload: EmailContent) => {
-    const transporter = nodemailer.createTransport({
-        service : 'Gmail',
-        auth: {
-            user : process.env.SOURCE_EMAIL,
-            pass : process.env.SOURCE_PASSWORD
-        }
-    });
-    return await transporter.sendMail(payload);
-    
-}
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'OAuth2',
+        user: process.env.SOURCE_EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken.token,
+    }
+});
+  return await transporter.sendMail(payload);
+};
