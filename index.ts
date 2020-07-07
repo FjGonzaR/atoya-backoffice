bc18a01459f7bd83e1574a48c22af058db691e7d
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2020-07-07 10:21:30 -0500

    Delted wrong build

--- index.ts
+++ index.ts
@@ -1,33 +1,33 @@
 import "reflect-metadata";
 import express from "express";
 import bodyParser from "body-parser";
-import path from 'path';
+import path from "path";
 //import cookieParser from 'cookie-parser';
-import cors  from 'cors';
-import router from './src/routes';
+import cors from "cors";
+import router from "./src/routes";
 import { createConnection } from "typeorm";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'front/build')));
+app.use(express.static(path.join(__dirname, "front/build")));
 app.use(cors());
 //app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-createConnection().then(connection => {
-  app.use('/', router);
-}).catch(reason => console.log(`Database connection failed ${reason}`));
+createConnection()
+  .then((connection) => {
+    app.use("/", router);
+  })
+  .catch((reason) => console.log(`Database connection failed ${reason}`));
 
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  app.listen(process.env.PORT || 5000);
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-
+app.listen(process.env.PORT || 5000);
